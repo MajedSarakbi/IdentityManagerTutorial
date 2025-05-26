2196f3758cf8f5446b5f744daa6a28537b165199
majed <sarakbimajed@gmail.com>
2025-05-27 02:31:24 +0300

    Add DbContext And Identity Service

--- IdentityManagerTutorial/Program.cs
+++ IdentityManagerTutorial/Program.cs
@@ -1,3 +1,8 @@
+using IdentityManagerTutorial.Data;
+using IdentityManagerTutorial.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace IdentityManagerTutorial
 {
     public class Program
@@ -7,6 +12,19 @@ namespace IdentityManagerTutorial
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option =>
+            {
+                option.Password.RequireLowercase=true;
+                option.Password.RequireUppercase=true;
+                option.Password.RequireDigit=true;
+                option.Password.RequiredLength=10;
+                option.SignIn.RequireConfirmedEmail=false;
+            }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+           
+
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();

871cb9ecafc2dbd164fa867e4194d9af48cb9ee0
majed <sarakbimajed@gmail.com>
2025-05-27 01:44:53 +0300

    Add project files.

---
+++ IdentityManagerTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityManagerTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
